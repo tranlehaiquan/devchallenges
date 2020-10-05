bd0a0f90e204176edf53d2e1c2736b4622258643
Q.Tran <tranlehaiquan@gmail.com>
2020-10-05 15:00:23 +0700

    update item clickable

--- github-jobs/src/components/JobItem/index.tsx
+++ github-jobs/src/components/JobItem/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import clsx from 'clsx';
+import { Link } from 'gatsby';
 
 import JobItemWrapper from './JobItemWrapper';
 import Image from '../Image';
@@ -9,78 +10,14 @@ import JobDescription from './JobDescription';
 
 const useStyles = makeStyles(({ spacing, palette, breakpoints }) => ({
   root: {
-    display: 'flex',
-    borderRadius: spacing(1),
-    boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.05)',
-    backgroundColor: palette.common.white,
-    padding: spacing(1.75),
-    [breakpoints.down('xs')]: {
-      display: 'block',
-    },
+    position: 'relative',
   },
-  logo: {
-    width: 120,
-    display: 'flex',
-    alignItems: 'center',
-    marginRight: spacing(1.75),
-    '& img': {
-      display: 'block',
-      maxWidth: '100%',
-    },
-    [breakpoints.down('xs')]: {
-      width: '100%',
-      justifyContent: 'flex-start',
-      marginBottom: spacing(1),
-      '& img': {
-        display: 'block',
-        width: '100%',
-      },
-    },
-  },
-  jobType: {
-    display: 'inline-block',
-    border: '1px solid black',
-    borderRadius: spacing(0.5),
-    padding: spacing(0.5, 1),
-    [breakpoints.down('sm')]: {
-      marginBottom: spacing(1),
-    },
-  },
-  jobInfos: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'flex-end',
-    marginTop: spacing(3),
-    [breakpoints.down('sm')]: {
-      display: 'block',
-      marginTop: spacing(1),
-    },
-  },
-  durationAndLocation: {
-    display: 'inline-flex',
-    color: palette.grey[500],
-    '& p': {
-      display: 'inline-flex',
-    },
-    [breakpoints.down('sm')]: {
-      display: 'block',
-    },
-  },
-  jobDes: {
-    flex: 1,
-  },
-  jobTime: {
-    display: 'inline-flex',
-    alignItems: 'center',
-    '&:not(:last-of-type)': {
-      marginRight: spacing(2),
-    },
-  },
-  title: {
-    textDecoration: 'none',
-  },
-  distanceToNow: {
-    marginLeft: spacing(1),
+  link: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    height: '100%',
+    width: '100%',
   },
 }));
 
@@ -101,20 +38,23 @@ const JobItem: React.FC<JobItemProps> = ({
   const classes = useStyles();
 
   return (
-    <JobItemWrapper
-      className={clsx(classes.root, className)}
-      logo={<Image src={company_logo} alt={company} loading="lazy" />}
-      description={
-        <JobDescription
-          location={location}
-          id={id}
-          company={company}
-          title={title}
-          created_at={created_at}
-          type={type}
-        />
-      }
-    />
+    <div className={classes.root}>
+      <JobItemWrapper
+        className={className}
+        logo={<Image src={company_logo} alt={company} loading="lazy" />}
+        description={
+          <JobDescription
+            location={location}
+            id={id}
+            company={company}
+            title={title}
+            created_at={created_at}
+            type={type}
+          />
+        }
+      />
+      <Link to={`/position/${id}`} className={classes.link}></Link>
+    </div>
   );
 };
 
